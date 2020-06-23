46faa621fa4244bfe6fdfa014dffe79f61ad36db
Syndamia <kamen.d.mladenov@protonmail.com>
2020-06-23 22:54:32 +0300

    Fixed inconsistent/bad formatting in Dispenser microcontroller code

--- Code from circuits/DispenserMicrocontroller/DispenserMicrocontroller.ino
+++ Code from circuits/DispenserMicrocontroller/DispenserMicrocontroller.ino
@@ -1,6 +1,14 @@
 #include <Servo.h>
 // Note: the library disables analogWrite on pins 9 and 10
 
+#define INPUT_PIN A0
+
+#define SERVO_1_PIN 3
+#define SERVO_2_PIN 5
+#define SERVO_3_PIN 6
+#define SERVO_4_PIN 9
+#define SERVO_5_PIN 10
+
 Servo servo_1;
 Servo servo_2;
 Servo servo_3;
@@ -18,17 +26,10 @@ void zeroOutServos() {
   servo_5.write(0);
 }
 
-#define INPUT_PIN A0
-#define SERVO_1_PIN 3
-#define SERVO_2_PIN 5
-#define SERVO_3_PIN 6
-#define SERVO_4_PIN 9
-#define SERVO_5_PIN 10
-
 void setup()
 {
   pinMode(INPUT_PIN, INPUT);
-  
+
   pinMode(SERVO_1_PIN, OUTPUT);
   servo_1.attach(SERVO_1_PIN);
   pinMode(SERVO_2_PIN, OUTPUT);
@@ -39,7 +40,7 @@ void setup()
   servo_4.attach(SERVO_4_PIN);
   pinMode(SERVO_5_PIN, OUTPUT);
   servo_5.attach(SERVO_5_PIN);
-  
+
   delay(2000);
   zeroOutServos();
 }
@@ -54,11 +55,22 @@ void loop()
 
 void dispensePill(int servoIndex) {
   switch(servoIndex) {
-    case 1: servo_1.write((servo_1.read() == 180)?0:180); break;
-    case 2: servo_2.write((servo_2.read() == 180)?0:180); break;
-    case 3: servo_3.write((servo_3.read() == 180)?0:180); break;
-    case 4: servo_4.write((servo_4.read() == 180)?0:180); break;
-    case 5: servo_5.write((servo_5.read() == 180)?0:180); break;
+  case 1: 
+    servo_1.write((servo_1.read() == 180)?0:180); 
+    break;
+  case 2: 
+    servo_2.write((servo_2.read() == 180)?0:180); 
+    break;
+  case 3: 
+    servo_3.write((servo_3.read() == 180)?0:180); 
+    break;
+  case 4: 
+    servo_4.write((servo_4.read() == 180)?0:180); 
+    break;
+  case 5: 
+    servo_5.write((servo_5.read() == 180)?0:180); 
+    break;
   }
   delay(1500);
 }
+
